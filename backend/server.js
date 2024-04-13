124871449160e1e43927298cfeac0c87fcdc4a07
Asdf123ah <0320-0770@lspu.edu.ph>
2024-04-13 15:41:20 +0800

    feat: new api for adminpage

--- backend/server.js
+++ backend/server.js
@@ -734,6 +734,18 @@ app.get("/api/user/:userId/categoryAttempt", async (req, res) => {
   }
 });
 
+app.get("/api/admin/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
